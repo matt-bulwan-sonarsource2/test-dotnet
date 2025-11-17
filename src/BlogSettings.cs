5d4919cd99e583e00624516d930e88f59b8e4214
Matt Bulwan <matt.bulwan@sonarsource.com>
2025-11-17 12:55:06 -0600

    Update BlogSettings.cs

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -2,7 +2,7 @@ namespace Miniblog.Core
 {
     public class BlogSettings
     {
-        public int CommentsCloseAfterDays { get; set; } = 18;
+        public int CommentsCloseAfterDays { get; set; } = 28;
 
         public bool DisplayComments { get; set; } = false;
 
